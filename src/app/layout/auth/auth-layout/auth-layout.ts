4e37b789c7bcd3111a987608b123df3e073c07a0
HendAdel1 <1hend.adel1@gmail.com>
2026-03-05 03:49:37 +0200

    routing, auth-layout, auth-sidebar to be continued...

--- src/app/layout/auth/auth-layout/auth-layout.ts
+++ src/app/layout/auth/auth-layout/auth-layout.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthSidebar } from "../auth-sidebar/auth-sidebar";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, AuthSidebar],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
 })
--- src/app/layout/auth/auth-sidebar/auth-sidebar.ts
+++ src/app/layout/auth/auth-sidebar/auth-sidebar.ts
@@ -1,11 +1,15 @@
+import { LucideAngularModule, Brain, BookOpenCheck, RectangleEllipsis, FolderCode } from 'lucide-angular';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth-sidebar',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './auth-sidebar.html',
   styleUrl: './auth-sidebar.css',
 })
 export class AuthSidebar {
-
+      readonly FolderCode = FolderCode;
+      readonly Brain = Brain;
+      readonly BookOpenCheck = BookOpenCheck;
+      readonly RectangleEllipsis = RectangleEllipsis;
 }
