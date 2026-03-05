ce6a72c91921de278ccc178cb7ec9b58b12b29c3
HendAdel1 <1hend.adel1@gmail.com>
2026-03-05 15:45:26 +0200

    avoid repeated line in auth-sidebar

--- src/app/layout/auth/auth-sidebar/auth-sidebar.ts
+++ src/app/layout/auth/auth-sidebar/auth-sidebar.ts
@@ -1,10 +1,11 @@
 import { BookOpenCheck, Brain, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, RectangleEllipsis} from 'lucide-angular';
 import { Component } from '@angular/core';
 import { Brand } from '../../../shared/components/brand/brand';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-sidebar',
-  imports: [LucideAngularModule, Brand],
+  imports: [LucideAngularModule, Brand, CommonModule],
   templateUrl: './auth-sidebar.html',
   styleUrl: './auth-sidebar.css',
   providers:[{
@@ -15,4 +16,21 @@ import { Brand } from '../../../shared/components/brand/brand';
 })
 export class AuthSidebar {
 
+  features = [
+    {
+      icon: 'brain',
+      title: 'Tailored Diplomas',
+      text: 'Choose from specialized tracks like Frontend, Backend, and Mobile Development.',
+    },
+    {
+      icon: 'book-open-check',
+      title: 'Focused Exams',
+      text: 'Access topic-specific tests including HTML, CSS, JavaScript, and more.',
+    },
+    {
+      icon: 'rectangle-ellipsis',
+      title: 'Smart Multi-Step Forms',
+      text: 'Choose from specialized tracks like Frontend, Backend, and Mobile Development.',
+    }
+  ]
 }
