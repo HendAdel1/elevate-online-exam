5061b85ed0f6c522d0e64f784872c580993c764d
HendAdel1 <1hend.adel1@gmail.com>
2026-03-07 03:14:51 +0200

    create-password, it's form

--- src/app/features/auth/pages/create-password/create-password.ts
+++ src/app/features/auth/pages/create-password/create-password.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthTitle } from '../../../../shared/ui/auth-title/auth-title';
+import { CreatePasswordForm } from '../../components/create-password-form/create-password-form';
+import { AuthLink } from '../../../../shared/ui/auth-link/auth-link';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-password',
-  imports: [],
+  imports: [AuthTitle, CreatePasswordForm, AuthLink, RouterLink],
   templateUrl: './create-password.html',
   styleUrl: './create-password.css',
 })
