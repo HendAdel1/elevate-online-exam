288cf4836902b7cc69441c32f207ce2c7931fbd4
HendAdel1 <1hend.adel1@gmail.com>
2026-03-28 17:37:21 +0200

    toggle password in create-password-form, create-new-password-form

--- src/app/features/auth/components/create-password-form/create-password-form.ts
+++ src/app/features/auth/components/create-password-form/create-password-form.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Eye, EyeOff, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider } from 'lucide-angular';
 import { AuthButton } from '../../../../shared/ui/auth-button/auth-button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-password-form',
@@ -14,5 +15,42 @@ import { AuthButton } from '../../../../shared/ui/auth-button/auth-button';
       }]
 })
 export class CreatePasswordForm {
+
+  loginForm: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
+  loginError: string | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+  
+  toggleConfirmPassword() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      // Mark all controls as touched to show validation errors
+      this.loginForm.markAllAsTouched();
+      this.loginError = 'Something went wrong';
+      return;
+    }
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.loginForm.get('confirmPassword');
+  }
 
 }
