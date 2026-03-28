72d900aea8b70f7479a135317d2b48d866eaf546
HendAdel1 <1hend.adel1@gmail.com>
2026-03-28 17:13:30 +0200

    routerlink in email

--- src/app/features/auth/pages/registeration/steps/email/email.ts
+++ src/app/features/auth/pages/registeration/steps/email/email.ts
@@ -1,10 +1,11 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 import { AuthLink } from "../../../../../../shared/ui/auth-link/auth-link";
 import { EmailForm } from "../../../../components/email-form/email-form";
 
 @Component({
   selector: 'app-email',
-  imports: [AuthLink, EmailForm],
+  imports: [AuthLink, EmailForm, RouterLink],
   templateUrl: './email.html',
   styleUrl: './email.css',
 })
