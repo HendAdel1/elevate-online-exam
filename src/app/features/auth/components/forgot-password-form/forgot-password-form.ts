b590951ffef7d75e2b80d2d59c14ca344620b4c8
HendAdel1 <1hend.adel1@gmail.com>
2026-04-30 14:54:52 -0700

    forgot-password logic work, add redirect url to 'forgor-password-request'

--- src/app/features/auth/components/forgot-password-form/forgot-password-form.ts
+++ src/app/features/auth/components/forgot-password-form/forgot-password-form.ts
@@ -1,11 +1,59 @@
 import { Component, Output } from '@angular/core';
 import { AuthButton } from '../../../../shared/ui/auth-button/auth-button';
+import { AuthInput } from '../../../../shared/ui/auth-input/auth-input';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../../../../projects/auth/src/lib/services/auth.service';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-forgot-password-form',
-  imports: [AuthButton],
+  imports: [AuthButton, AuthInput, ReactiveFormsModule],
   templateUrl: './forgot-password-form.html',
   styleUrl: './forgot-password-form.css',
 })
 export class ForgotPasswordForm {
+
+    
+form = new FormGroup({
+  email: new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.email],
+  }),
+});
+
+get emailControl() {
+  return this.form.controls.email;
+}
+
+  isSubmitting = false;
+  errorMessage: string | null = null;
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onSubmit() {
+    if (this.emailControl.invalid) {
+      this.emailControl.markAsTouched();
+      return;
+    }
+
+    const email = this.emailControl.value.trim().toLowerCase();
+
+    this.isSubmitting = true;
+    this.errorMessage = null;
+
+    this.authService.forgotPassword({email, redirectUrl: `${window.location.origin}/auth/create-new-password`}).pipe(finalize(() => this.isSubmitting = false))
+    .subscribe((res) =>{
+      this.router.navigate(['/auth/verify-email'])
+
+      if(!res.resetToken){
+        this.errorMessage = res.message || 'Something went wrong';
+        return;
+      }
+    });
+
+  }
 }
