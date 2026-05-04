24409ba0821d446515b7a0ee712d4c29d2aa3976
HendAdel1 <1hend.adel1@gmail.com>
2026-05-04 05:55:51 -0700

    sidenav-nav-links ui

--- src/app/layout/dashboard/components/sidenav/components/sidenav-nav-links/sidenav-nav-links.ts
+++ src/app/layout/dashboard/components/sidenav/components/sidenav-nav-links/sidenav-nav-links.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { GraduationCap, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, UserRound } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidenav-nav-links',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './sidenav-nav-links.html',
   styleUrl: './sidenav-nav-links.css',
+   providers:[{
+        provide: LUCIDE_ICONS,
+        multi: true,
+        useValue: new LucideIconProvider({GraduationCap, UserRound}),
+      }]
 })
 export class SidenavNavLinks {
+
+  activeRoute = 'diplomas';
+
+  links = [
+    { name: 'Diplomas', icon: 'graduation-cap', route: 'diplomas' },
+    { name: 'Account Settings', icon: 'user-round', route: 'account-settings' }
+  ];
+
 
 }
