35beabba0b2a55ec9d9fb98130b33dc965369716
HendAdel1 <1hend.adel1@gmail.com>
2026-05-04 09:42:56 -0700

    sidenav-footer user info & dropdown ui

--- src/app/layout/dashboard/components/sidenav/components/sidenav-footer/sidenav-footer.ts
+++ src/app/layout/dashboard/components/sidenav/components/sidenav-footer/sidenav-footer.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Bolt, EllipsisVertical, LogOut, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, UserRound } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidenav-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './sidenav-footer.html',
   styleUrl: './sidenav-footer.css',
+  providers: [{
+    provide: LUCIDE_ICONS,
+    multi: true,
+    useValue: new LucideIconProvider({ EllipsisVertical, UserRound, Bolt, LogOut }),
+  }]
 })
 export class SidenavFooter {
 
+  isOpen = false;
+
+  @HostListener('document:click', ['$event'])
+  closeOnOutsideClick(event: Event) {
+    this.isOpen = false;
+  }
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
 }
