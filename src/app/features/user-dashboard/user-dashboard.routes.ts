a2dd65d167d28ad8d72a15e9e1c5bc7b188f8676
HendAdel1 <1hend.adel1@gmail.com>
2026-05-04 15:52:03 -0700

    dashboard-header with back button depends on routes 'showBack', edit padding of header and breadcrumb

--- src/app/features/user-dashboard/user-dashboard.routes.ts
+++ src/app/features/user-dashboard/user-dashboard.routes.ts
@@ -13,37 +13,55 @@ export const userDashboardRoutes: Routes = [
       },
       {
         path: 'diplomas',
-        data: { breadcrumb: 'Diplomas' },
+        data: { 
+          breadcrumb: 'Diplomas',
+          header: { icon: 'graduation-cap', label: 'Diplomas', showBack: false}
+         },
         loadComponent: () =>
           import('./pages/diplomas/diplomas').then(m => m.Diplomas)
       },
       {
         path: 'diplomas/:diplomaId/exams',
-        data: { breadcrumb: 'Exams' },
+        data: { 
+          breadcrumb: 'Exams',
+          header: { icon: 'book-open-check', type: 'diplomaExams', showBack: true}
+         },
         loadComponent: () =>
           import('./pages/exams/exams').then(m => m.Exams)
       },
       {
         path: 'diplomas/:diplomaId/exams/:examId/questions',
-        data: { breadcrumb: 'Questions' },
+        data: { 
+          breadcrumb: 'Questions',
+          header: { icon: 'circle-question-mark', type: 'examQuestions', showBack: true}
+         },
         loadComponent: () =>
           import('./pages/questions/questions').then(m => m.Questions)
       },
       {
         path: 'diplomas/:diplomaId/exams/:examId/questions/:questionId/answers',
-        data: { breadcrumb: 'Answers' },
+        data: { 
+          breadcrumb: 'Answers',
+          header: { icon: 'message-circle', type: 'answers', showBack: true}
+         },
         loadComponent: () =>
           import('./pages/answers/answers').then(m => m.Answers)
       },
       {
         path: 'account',
-        data: { breadcrumb: 'Account' },
+        data: { 
+          breadcrumb: 'Account',
+          header: { icon: 'user-round', label: 'Account Settings', showBack: true}
+         },
         loadComponent: () =>
           import('./pages/account/account').then(m => m.Account)
       },
       {
         path: 'change-password',
-        data: { breadcrumb: 'Change Password' },
+        data: { 
+          breadcrumb: 'Change Password',
+          header: { icon: 'key-round', label: 'Account Settings', showBack: true}
+         },
         loadComponent: () =>
           import('./pages/change-password/change-password').then(m => m.ChangePassword)
       }
--- src/app/layout/dashboard/components/dashboard-header/dashboard-header.ts
+++ src/app/layout/dashboard/components/dashboard-header/dashboard-header.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { BookOpenCheck, ChevronLeft, CircleQuestionMark, GraduationCap, KeyRound, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, MessageCircle, UserRound } from 'lucide-angular';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './dashboard-header.html',
   styleUrl: './dashboard-header.css',
+  providers: [{
+    provide: LUCIDE_ICONS,
+    multi: true,
+    useValue: new LucideIconProvider({ GraduationCap, BookOpenCheck, CircleQuestionMark, MessageCircle, UserRound, KeyRound, ChevronLeft }),
+  }]
 })
-export class DashboardHeader {
+export class DashboardHeader implements OnInit {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
 
+  title = '';
+  icon = '';
+  showBack = false;
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => this.updateHeader());
+
+    this.updateHeader();
+  }
+
+  private getDeepestRoute(route: ActivatedRoute): ActivatedRoute {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
+
+  private updateHeader() {
+    const current = this.getDeepestRoute(this.route);
+    const data = current.snapshot.data['header'];
+    const params = current.snapshot.params;
+
+    if (!data) {
+      this.reset();
+      return;
+    }
+
+    this.icon = data.icon;
+    this.showBack = data.showBack ?? false;
+    this.title = this.getTitle(data, params);
+  }
+
+  private reset() {
+    this.title = '';
+    this.icon = '';
+    this.showBack = false;
+  }
+
+  private getTitle(data: any, params: any): string {
+    if (data.type === 'diplomaExams') {
+      return `Diploma ${params['diplomaId']} Exams`;
+    }
+
+    if (data.type === 'examQuestions') {
+      return `Exam ${params['examId']} Questions`;
+    }
+
+    if (data.type === 'answers') {
+      return 'Answers';
+    }
+
+    return data.label || '';
+  }
 }
