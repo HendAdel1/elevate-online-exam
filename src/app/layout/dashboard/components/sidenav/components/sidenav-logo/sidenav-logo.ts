6ed51b4117f2f34fb5852dfdd60fdc5cbc72d6d5
HendAdel1 <1hend.adel1@gmail.com>
2026-05-04 03:02:34 -0700

    rename assests to assets, sidenav-logo, sidenav

--- src/app/layout/dashboard/components/sidenav/components/sidenav-logo/sidenav-logo.ts
+++ src/app/layout/dashboard/components/sidenav/components/sidenav-logo/sidenav-logo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Brand } from "../../../../../../shared/components/brand/brand";
 
 @Component({
   selector: 'app-sidenav-logo',
-  imports: [],
+  imports: [Brand],
   templateUrl: './sidenav-logo.html',
   styleUrl: './sidenav-logo.css',
 })
--- src/app/layout/dashboard/components/sidenav/sidenav.ts
+++ src/app/layout/dashboard/components/sidenav/sidenav.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { SidenavLogo } from "./components/sidenav-logo/sidenav-logo";
+import { SidenavNavLinks } from "./components/sidenav-nav-links/sidenav-nav-links";
+import { SidenavFooter } from "./components/sidenav-footer/sidenav-footer";
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [SidenavLogo, SidenavNavLinks, SidenavFooter],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })
